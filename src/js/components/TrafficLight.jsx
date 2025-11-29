656a41d73c9d9ad6474dce6636ed06a05698c4ff
1989212 <jessica_villamizar.c@hotmail.com>
2025-11-29 04:11:06 +0000

    inicio-proyecto

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Trafficlight = () => {
+	return <div className="container fluid bg-dark p-4 ">
+	<div class="btn-group-vertical d-flex justify-content align-items" role="group" aria-label="Basic mixed styles example">
+  <button type="button" class="btn btn-danger">Left</button>
+  <button type="button" class="btn btn-warning">Middle</button>
+  <button type="button" class="btn btn-success">Right</button>
+</div>
+</div>
+
+
+};
+
+export default Trafficlight;
